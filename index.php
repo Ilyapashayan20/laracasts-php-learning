83b4c9d3c9b7265415192af069c76f1056f1e589
ilyapashayan <ilya.w.a.j@gmail.com>
2024-06-11 03:13:57 +0400

    environments and configuration flexinility

--- index.php
+++ index.php
@@ -1,54 +1,13 @@
 <?php
 
 require 'functions.php';
-// require 'router.php';
+require 'Database.php';
+$config = require 'config.php';
 
 
-// class Person  { 
-//     public $name;
-//     public $age;
+$db = new Database($config['database']);
 
-//     public function breathe(){
-//         echo  $this->name . ' is breathing';
-//     }
-// }
-
-// $person = new Person();
-
-// $person->name = 'Ilya';
-// $person->age = 20;
-// $person->breathe();
-
-class Database
-{
-
-    public $connection;
-
-    public function __construct()
-    {
-
-        $dsn = "mysql:host=localhost;port=3306;dbname=demo_db;charset=utf8mb4";
-
-        $this->connection = new PDO($dsn, 'root');
-
-    }
-    public function query($query)
-    {
-
-        $statement = $this->connection->prepare($query);
-
-        $statement->execute();
-
-        return $statement->fetchAll();
-
-
-    }
-}
-
-
-$db = new Database();
-
-$posts = $db->query("SELECT * FROM posts where id > 1");
+$posts = $db->query("SELECT * FROM posts")->fetchAll();
 
 
 foreach ($posts as $post) {
