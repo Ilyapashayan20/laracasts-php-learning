cd3f1ed2b03f1d23bd3c1c9e11b7f2449c0bb36a
ilyapashayan <ilya.w.a.j@gmail.com>
2024-06-15 21:35:49 +0400

    Fix issues in router.php and notes.view.php, add new routes and note-create view

--- views/notes.view.php
+++ views/notes.view.php
@@ -4,9 +4,15 @@
 
 <main>
     <div class="mx-auto max-w-7xl flex flex-col gap-2 py-6 sm:px-6 lg:px-8">
+       <ul>
        <?php foreach ($notes as $note) :?>
         <a  class=' underline text-blue-500' href="/note?id=<?= $note['id']?>"><?= $note['body'] ?></a>
         <?php  endforeach; ?>
+       </ul>
+
+       <p class="mt-6">
+        <a class=" text-blue-500 hover:underline" href="/notes/create"> Create Note</a>
+       </p>
     </div>
 </main>
 
