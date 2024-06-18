7ec808494316109b768789cc790518a680f4a6e8
ilyapashayan <ilya.w.a.j@gmail.com>
2024-06-18 22:04:50 +0400

    insert data to db , and escape untrusted input

--- views/notes.view.php
+++ views/notes.view.php
@@ -4,9 +4,9 @@
 
 <main>
     <div class="mx-auto max-w-7xl flex flex-col gap-2 py-6 sm:px-6 lg:px-8">
-       <ul>
+       <ul class=" flex flex-col gap-4">
        <?php foreach ($notes as $note) :?>
-        <a  class=' underline text-blue-500' href="/note?id=<?= $note['id']?>"><?= $note['body'] ?></a>
+        <a  class=' underline text-blue-500' href="/note?id=<?= $note['id']?>"><?= htmlspecialchars($note['body']) ?></a>
         <?php  endforeach; ?>
        </ul>
 
