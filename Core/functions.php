d6ab8abacd77a43b37c8b60726719cfe57df2aed
ilyapashayan <ilya.w.a.j@gmail.com>
2024-06-28 21:02:08 +0400

    Extract a Form Validation Object

--- Core/functions.php
+++ Core/functions.php
@@ -45,21 +45,9 @@ function view($path,$attributes = [])
 }
 
 
-function login($user)
+
+function redirect($path)
 {
-    $_SESSION['user'] = [
-        'email' => $user['email']
-    ];
-
-    session_regenerate_id(true);
-}
-
-function logout()
-{
-    $_SESSION = [];
-    session_destroy();
-
-    $params = session_get_cookie_params();
-    setcookie('PHPSESSID', '', time() - 3600, $params['path'], $params['domain'], $params['secure'], $params['httponly']);
-
+    header("location: {$path}");
+    exit();
 }
